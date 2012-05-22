bf462b473f0dd6253872f026f4e17314418d0788
Anthony <TonyT429@gmail.com>
2012-05-22 15:45:20 -0700

    got my current functions documented - except #8 which will wait until I fix it.

--- tonysLib.js
+++ tonysLib.js
@@ -13,12 +13,11 @@ var tonysLib = function () {
      };
      
 //    REGEX BREAKDOWN EXPLANATION:
-//    / - begins and ends the string,   ^  - matches beginning of input,  \ - indicates next character is special and not interpreted literally,  ? - Match 
-//    preceding character 0 or 1 time.  (\d{3})  - this is the area code between parans. \d = any digit, 0-9 and {3} indicates to do there are 3 digits.  
-//     [-. ]  indicates that the 3 digits in the area code are separated from the city code by either a dash, a period, or a space.  This patter continues
-//    for the next 3 digits of the city exchange code and then the station code, which has 4 digits .  Finally, the $ indicates the end of input (opposite
-//    of the ^), and the / to close the string. 
- 
+//    / -  at the beginning and end of the string,   ^  - matches beginning of input,  \ - indicates next character is special and not interpreted literally,  
+//    ? - Match preceding character 0 or 1 time.  (\d{3})  - this is the area code between parans. \d = any digit, 0-9 and {3} indicates to do there 
+//    are 3 digits.   [-. ]  indicates that the 3 digits in the area code are separated from the city code by either a dash, a period, or a space.  This 
+//    pattern continues for the next 3 digits of the city exchange code and then the station code, which has 4 digits .  Finally, the $ indicates the end
+//    of input (opposite of the ^), and the / to close the string. 
      
      
 //    04 - Title-case A String, Split Into Words, Then Uppercase The First Letter Of Each Word.
@@ -26,13 +25,16 @@ var tonysLib = function () {
      function propCase (val) {
 	      var strChg = val;
                return strChg.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-     };      //  
+     };      
+     
+//  / - at the beginning and end of the string.  \w - matches any alphanumeric character including underscore.  \S matches a single character.
+//  * - matches the preceding character 0 or more times.  The g, outside the string means to do the match globally throughout the string.
      
 
 //	  05 - Given A String That Is A List Separated By ","s Change The Separator To Another Separator Such As "/".
      
      function myStr (val) {
-          var re = /,/gi;  
+          var re = /,/gi;                //  variable re is set to the character ,   the gi says to match this globally throughout the string and ignore the case.
           var getStr = val;
                var newStr = getStr.replace(re, "/");
                return newStr;
@@ -43,8 +45,8 @@ var tonysLib = function () {
     
     function decimal2 (val) { 
          var chgNum = val;
-         var newNum = chgNum.toFixed(2);  
-              return ("$" + newNum);	 
+         var newNum = chgNum.toFixed(2);    // sets the decimal to 2 places
+              return ("$" + newNum);	                 // adds the $ to precede the results to format to US currency.
      };
      
      
@@ -66,6 +68,9 @@ var tonysLib = function () {
          var isNum = parseInt(val)
               return isNum;
     };
+    
+    
+//     RETURN VALUES
     
     return {
          "isAPhoneNum": isAPhoneNum,
