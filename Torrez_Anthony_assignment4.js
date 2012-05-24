f932f3bdf13143172ca028c06f7e0c3a765fbae3
Anthony <TonyT429@gmail.com>
2012-05-24 00:05:39 -0700

    Got problem 3 to work and documented!

--- Torrez_Anthony_assignment4.js
+++ Torrez_Anthony_assignment4.js
@@ -5,6 +5,7 @@ var lib = new tonysLib();
 
 // Check 01 - Does A String Follow A Phone Number Pattern?
 
+console.log("Problem 1");
 console.log( lib.isAPhoneNum("9225-454-5265"));
 console.log( lib.isAPhoneNum("925-4554-5265"));
 console.log( lib.isAPhoneNum("925-454-52265"));
@@ -15,26 +16,45 @@ console.log( lib.isAPhoneNum("925-454-525"));
 
 // Check 02 - Does a string follow an aaa@bbb.ccc pattern like an email address?
 
+console.log("Problem 2");
 console.log( lib.isAnEmailAddr('TonyT429@fullsail.edu'));
 console.log( lib.isAnEmailAddr('TonyT429#fullsail.edu'));
 console.log( lib.isAnEmailAddr('a.torrez@comcast.net'));
 console.log( lib.isAnEmailAddr('anthony-torrez@comcast,org'));
 console.log( lib.isAnEmailAddr('TonyT-429@fullsail.edu'));
 
+//  03 - Is The String A URL? 
+
+console.log("Problem 3");
+console.log( lib.urlChk("https://www.go-pro.com"));
+console.log( lib.urlChk("http://www.comcast.net"));
+console.log( lib.urlChk("http:\\www.go-pro.com"));
+console.log( lib.urlChk("www.go-pro.com"));
+console.log( lib.urlChk("go-pro.com"));
+console.log( lib.urlChk("nttp://www.go-pro.com"));
+console.log( lib.urlChk("http;//.www.go-pro.com"));
+console.log( lib.urlChk("http://www.lukew.com/ff/entry.asp?1071"));
+
 // Check 04 - Title-case A String, Split Into Words, Then Uppercase The First Letter Of Each Word.
 
+console.log("Problem 4");
 console.log( lib.propCase("this is a test"));
 
 // Check 05 - Given A String That Is A List Separated By ","s Change The Separator To Another Separator Such As "/".
 
+console.log("Problem 5");
 console.log( lib.myStr("123,456,789,10,11,12,13,14"));
 
 // Check 06 - Format A Number To Use A Specific Number Of Decimal Places, As For Money: 2.1 -> 2.10
 
+console.log("Problem 6");
 console.log( lib.decimal2(987.2349));
+
+// 07) Fuzzy-match a number: is the number above or below a number within a certain percent?
 
 // Check 09 - Given A String Version Of A Number Such As "42", Return A Value As An Actual Number, Such As 42.  
 
+console.log("Problem 9");
 console.log( lib.numChk(42));
 
 // Check 08 - Find The Number Of Days Difference Between 2 Dates.
--- tonysLib.js
+++ tonysLib.js
@@ -1,11 +1,13 @@
 var tonysLib = function () { 
 
+
+// STRING:
+
 //    01 - Does A String Follow A Phone Number Pattern? 
 
      function isAPhoneNum (val) {
 	      var regexPhone = /^\(?(\d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$/;                           // See  REGEX BREAKDOWN EXPLANATION     
-		  if (regexPhone.test(val)) {                                                                           // NESTED CONDITIONAL INSIDE FUNCTION 
-//    			var formattedPhoneNumber = val.replace(regexPhone, "$1-$2-$3");      
+		  if (regexPhone.test(val)) {                                                                           // NESTED CONDITIONAL INSIDE FUNCTION      
         		return ("Yes, " + val + " follows a phone number pattern.");
 		 } else {
    		 return ("Nope, " + val + " does not follow a phone number pattern");
@@ -23,8 +25,8 @@ var tonysLib = function () {
 //    02 - Does A String Follow An aaa@bbb.ccc Pattern Like An Email Address?
 
 	function isAnEmailAddr (val) {
-	      var regexEmail = /[\w.-]+@[\w,-]+\.[\w.-]+/;       // See  REGEX BREAKDOWN EXPLANATION 
-		 	    if (regexEmail.test(val)) {                                                                             // NESTED CONDITIONAL INSIDE FUNCTION      
+	      var regexEmail = /[\w.-]+@[\w,-]+\.[\w.-]+/;                                          // See  REGEX BREAKDOWN EXPLANATION 
+		 	    if (regexEmail.test(val)) {                                                               // NESTED CONDITIONAL INSIDE FUNCTION      
         		return ("Yes, " + val + " follows a Email number pattern.");
 		  } else {
    		 		return ("Nope, " + val + " does not follow a Email number pattern");
@@ -39,7 +41,22 @@ var tonysLib = function () {
 
 //    03 - Is The String A URL? 
 
+	function urlChk(val) {
+    	var siteChk = new RegExp();                                                                              // CREATE A NEW REGULAR EXPRESSION AND COMPILE IT
+    	    siteChk.compile("^(http(s?)\:\/\/)?[\w-]+\.[A-Za-z0-9-_%&\?\/.=]+$");       // See  REGEX BREAKDOWN EXPLANATION 
+          	if(siteChk.test(val)) {                                                                                            // NESTED CONDITIONAL INSIDE FUNCTION
+            	return (val + ' Yes, this follows a URL pattern');
+            } else {
+               return (val + ' No, this does\'t appear to follow a URL pattern');
+    	}
+	};
 
+//    REGEX BREAKDOWN EXPLANATION:
+//    Because it is being called as a string, had to put it in parans with quotes.  ^  - matches beginning of input and $ ends the input.  In parans, we 
+//    check for http or https.  ? say match preceding character 0 or 1 time.  The \ indicates that the next character is not to be take literally, this is 
+//    done once for the : and both /'s.  In brackets [ ], the \w indicates any character such as A-Za-z0-9_  added to this is the dash -.  The + says to 
+//    match the preceding characters in brackets 1 or more times. The \ indicates that the next character, the period . is not to be take literally.  In the 
+//    brackets, we pass the characters A-Za-z0-9-_%&?/. and =.  
      
      
 //    04 - Title-case A String, Split Into Words, Then Uppercase The First Letter Of Each Word.
@@ -63,6 +80,8 @@ var tonysLib = function () {
     };
 
 
+// NUMBER:
+
 //     06 - Format A Number To Use A Specific Number Of Decimal Places, As For Money: 2.1 -> 2.10
     
     function decimal2 (val) { 
@@ -70,6 +89,9 @@ var tonysLib = function () {
          var newNum = chgNum.toFixed(2);    // sets the decimal to 2 places
               return ("$" + newNum);	                 // adds the $ to precede the results to format to US currency.
      };
+     
+     
+//    07 - Fuzzy-match A Number: Is The Number Above Or Below A Number Within A Certain Percent?
      
      
 //    08 - Find The Number Of Days Difference Between 2 Dates.
@@ -92,11 +114,24 @@ var tonysLib = function () {
     };
     
     
+// ARRAY
+
+//   10 - Find The Smallest Value In An Array That Is Greater Than A Given Number.
+
+//   11 - Find The Total Value Of Just The Number In An Array, Even If Some Of The Items Are Not Numbers.
+
+//   12 - Given An Array Of Objects And The Name Of A Key, Return The Array Sorted By The Value Of 
+//          That Key In Each Of The Objects: "a" + [{a:2}, {a:3}, {a:1}] -> [{a:1}, {a:2}, {a:3}].
+
+
+
+    
 //     RETURN VALUES
     
     return {
          "isAPhoneNum": isAPhoneNum,
          "isAnEmailAddr": isAnEmailAddr,
+         "urlChk": urlChk,
          "propCase": propCase,
          "myStr": myStr,
          "decimal2": decimal2,
@@ -104,3 +139,5 @@ var tonysLib = function () {
          "numChk": numChk
     }
 };
+
+
